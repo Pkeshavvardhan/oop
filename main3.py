48bbd23e5b1989a2cb3f8d7e7e1d3a27e6d92750
Pkeshavvardhan <keshav1008podar@gmail.com>
2025-04-02 17:56:27 +0530

    Tom and Jerry

---
+++ main3.py
@@ -0,0 +1,15 @@
+class robots:
+    name1="Tom"
+    name2="Jerry"
+    def introduction(self):
+
+        print("Bonjour Monsieur")
+
+    def skills(self):
+        print("my name is", self.name1)
+        print("my name is", self.name2)
+
+ob=robots()
+ob.introduction()
+ob.skills()
+
